4f9fa68088d5c2837f80fe610cf72478c323e825
BHUVANESWARI BABU <info.bhuvaneswari@gmail.com>
2026-01-30 14:02:30 +0000

    First commit after checking with supabase and postman and preparing to deploy in render

---
+++ client/src/main.js
@@ -0,0 +1,61 @@
+
+// fetch messages
+
+const display = document.getElementById('app')
+const form = document.getElementById('form')
+// const baseURL = 'https://deploying-and-git-server-2kxl.onrender.com'
+
+
+async function fetchData() {
+  // const response = await fetch(`${baseURL}/messages`)
+  const response = await fetch(`http://localhost:4242/guestbook`)
+  const messages = await response.json()
+
+  console.log(messages)
+
+  return messages
+}
+
+
+
+async function displayMessages() {
+  const messages = await fetchData()
+
+  messages.forEach((message) => {
+    const div = document.createElement('div')
+    const userName = document.createElement('p')
+    const messageContent = document.createElement('p')
+    const timeCreated=document.createElement(`small`)
+    
+
+    userName.textContent = message.msg_name
+    messageContent.textContent = message.content
+    timeCreated.textContent = new Date(message.created_at).toLocaleString()
+
+    div.append(userName, messageContent, timeCreated)
+
+    display.appendChild(div)
+  })
+}
+displayMessages()
+
+
+async function handleSubmit(event) {
+  event.preventDefault()
+
+  const formData = new FormData(form)
+  const userInput = Object.fromEntries(formData)
+  const userInputJSON = JSON.stringify(userInput)
+
+  const response = await fetch(`http://localhost:4242/guestbook`, {
+    headers: {
+      "Content-Type" : "application/json"
+    },
+    method: "POST",
+    body: userInputJSON
+  })
+  window.location.reload()
+} 
+
+form.addEventListener('submit', handleSubmit)
+console.log("FORM SUBMITTED VIA JS")
---
+++ server/server.js
@@ -0,0 +1,58 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import pg from "pg"
+    
+const app = express()
+app.use(express.json())
+app.use(cors())
+dotenv.config()
+
+const db = new pg.Pool({
+    connectionString: process.env.DB_CONN
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello')
+})
+
+app.get('/guestbook', async (req, res) => {
+    const data = await db.query(`SELECT * FROM guestbook`)
+    const messages = data.rows
+    res.status(200).json(messages)
+})
+
+// app.post('/guestbook', async (req, res) => {
+//   try {
+//     if (!req.body) {
+//       return res.status(400).json({ error: "No request body received" })
+//     }
+//     const { msg_name, content } = req.body
+
+//     if (!msg_name || !content) {
+//       return res.status(400).json({ error: "Name and message are required" })
+//     }
+
+//     await db.query(
+//       `INSERT INTO guestbook (msg_name, content) VALUES ($1, $2)`,
+//       [msg_name, content]
+//     )
+
+//     res.status(201).json({ message: "added message" })
+//   } catch (err) {
+//     console.error(err)
+//     res.status(500).json({ error: err.message })
+//   }
+// })
+
+
+app.post('/guestbook', async (req, res) => {
+    const userData = req.body
+    const dbQuery = await db.query(`INSERT INTO guestbook (msg_name, content) VALUES ($1, $2)`, [userData.msg_name, userData.content])
+
+    res.status(200).json({message: "added message"})
+})
+
+app.listen(4242, () => {
+    console.log(`Server started on port http://localhost:4242`)
+})
